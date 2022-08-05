6a591c8c1fe760971e58c6c4d64303123baaa34e
GullyBurns <GullyBurns>
2022-08-05 20:28:37 +0000

    Added checks to Pubmed search functions

--- databricks/utils/00_databricks_utils.py
+++ databricks/utils/00_databricks_utils.py
@@ -34,3 +34,7 @@ def dump_data_to_disk(df, file_stem, databricks_url='https://ie-meta-prod-databr
     name = name_match.group(1)
   print(name + ': '+url)
   displayHTML('<a href="'+url+'" >LINK</a>' )
+
+# COMMAND ----------
+
+show_doc(dump_data_to_disk)
