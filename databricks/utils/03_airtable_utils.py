d29bc57b4a87656b158b142873e1d21f94e4419f
GullyBurns <GullyBurns>
2022-09-03 05:24:31 +0000

    Added curatedDataUtils for reading curated data files

--- databricks/utils/03_airtable_utils.py
+++ databricks/utils/03_airtable_utils.py
@@ -18,6 +18,10 @@ import json
 from urllib.parse import quote
 from tqdm import tqdm
 import requests
+import nltk
+from nltk.metrics import agreement
+from nltk.metrics.agreement import AnnotationTask
+from nltk.metrics import masi_distance, binary_distance
 
 class AirtableUtils:
   """This class permits simple input / output from airtable
@@ -30,6 +34,7 @@ class AirtableUtils:
     """ Initialize the interface with an API key. 
     """
     self.api_key = api_key
+    self.task = None
     
   def _get_airtable_url(self, file, table):
     return 'https://api.airtable.com/v0/%s/%s?api_key=%s'%(file, table, self.api_key) 
@@ -134,6 +139,107 @@ class AirtableUtils:
       r = requests.post(url, headers=headers, data=payload)   
       print(r.text)
       
+
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
+  def build_nltk_annotation_task_from_curated_df(self, df, 
+                                                 doc_id_column, 
+                                                 category_column, 
+                                                 curator_column,
+                                                 distance_function=masi_distance):
+    document_task_data = []
+    curators = {}
+    docs = {}
+    categories = {}
+
+    for i, row in df.iterrows():
+      category_array = str(row[category_column]).split(',')
+      item = str(row[doc_id_column])
+      td_row = (row[curator_column], item, frozenset(category_array))
+      document_task_data.append(td_row)
+      if docs.get(item) is None:
+        docs[item] = 1
+      else:
+        docs[item] = docs[item] + 1
+
+      if curators.get(row[curator_column]) is None:
+        curators[row[curator_column]] = 1
+      else:
+        curators[row[curator_column]] = curators[row[curator_column]] + 1
+      for c in str(row[category_column]).split(','):
+        if categories.get(c) is None:
+          categories[c] = 1
+        else:
+          categories[c] = categories[c] + 1
+
+    doc_task = AnnotationTask(distance = distance_function)
+    doc_task.load_array(document_task_data)
+    self.docs = docs
+    self.curators
+    return docs, curators, categories, doc_task
+  
+  def _get_avg_doc_agr(item, curators, task):
+    temp_list = [] 
+    sum = 0.0
+    cnt = 0.0
+    for i in range(len(curators)):
+      for j in range(i):
+        try:
+          sum += task.agr(curators[i], curators[j], str(item))
+          cnt += 1.0
+        except StopIteration:
+          # No need to do anything - we get this error if attempting to compute agreement 
+          # between curators where one of them never entered a score. 
+          print('', end = '')
+    if cnt > 0.0:
+      avg = sum/cnt
+    else: 
+      avg = 0.0;
+    return avg
+
+  def _get_consensus(self, item, curators, task):
+    """ 
+    """
+    result = []
+    best = 0.0 
+    for i in range(len(curators)):
+      for j in range(i):
+        try:
+          agr = task.agr(curators[i], curators[j], str(item))
+          if agr == 1.0:
+            l = [x for x in doc_task.data if x['coder']==curators[i] and x['item']==item]
+            return list(l[0]['labels'])[0]
+        except StopIteration:
+          # No need to do anything - we get this error if attempting to compute agreement 
+          # between curators where one of them never entered a score. 
+          print('', end = '')
+    return None
+
+  def get_consensus_per_doc(df, task):
+
+    cat_list = sorted(list({c:0 for cc in df.CATEGORIES for c in cc.split(',')}.keys()))
+    curators = df.CURATOR.unique()
+
+    item_curator_dict = {cc: {c: '' for c in curators} for cc in df.ID_PAPER}
+    for row in df.itertuples():
+      item_curator_dict[row.ID_PAPER][row.CURATOR] = row.CATEGORIES
+
+    sdf = df.drop(['DISEASE_NAME', 'URI', 'TIMESTAMP','CATEGORIES','IRRELEVANT', \
+                   'COMMENTS','CURATOR', 'WORKSHEET', \
+                   'TITLE','ABSTRACT'], axis=1).drop_duplicates()
+    sdf = sdf.reset_index(drop=True)
+
+    #cat_count_dict = {cc: {c: 0 for c in cat_list} for cc in df.ID_PAPER}
+    #for row in df.itertuples():
+    #  for t in row.CATEGORIES.split(','):
+    #    cat_count_dict[row.ID_PAPER][t] = cat_count_dict.get(row.ID_PAPER).get(t) + 1
+    #cat_counts = [[cat_count_dict[row.ID_PAPER][c] for c in cat_list ] for row in sdf.itertuples()]
+    #sdf['CATEGORY_COUNTS'] = cat_counts
+
+    sdf['AVG_AGREEMENT'] = [get_avg_doc_agr(str(row.ID_PAPER), curators, task) for row in sdf.itertuples()]
+    sdf['CONSENSUS'] = [get_consensus(str(row.ID_PAPER), curators, task) for row in sdf.itertuples()]
+
+    return sdf
+
 
 # COMMAND ----------
 
