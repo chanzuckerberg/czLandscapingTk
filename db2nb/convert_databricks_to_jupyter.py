49aa11c4295a19393ccbe2b7d598482fd8f993b6
Gully Burns <gully.burns@chanzuckerberg.com>
2022-11-29 09:12:08 -0800

    Translate the databricks directory structure to nbdev

--- db2nb/convert_databricks_to_jupyter.py
+++ db2nb/convert_databricks_to_jupyter.py
@@ -130,9 +130,12 @@ def convert_databricks_nb(in_file, out_file):
 
 def main():
    for root, dirs, files in os.walk("databricks/", topdown=False):
+      new_root = re.sub('^databricks/', 'nbdev/', root)
+      if os.path.exists(new_root) is False:
+         os.makedirs(new_root)
       for name in files:
          ipynb_name = re.sub('.py', '.ipynb', name)
-         convert_databricks_nb(os.path.join(root, name), ipynb_name)
+         convert_databricks_nb(os.path.join(root, name), os.path.join(new_root, ipynb_name))
 
 if __name__ == '__main__':
     main()
