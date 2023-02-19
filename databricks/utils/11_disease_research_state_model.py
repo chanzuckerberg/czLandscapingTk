4d2d73bc1efd5c86e01175ac456def2822817494
GullyBurns <GullyBurns>
2023-02-19 04:56:06 +0000

    Fixing recursion in nvdev environment

--- databricks/utils/11_disease_research_state_model.py
+++ databricks/utils/11_disease_research_state_model.py
@@ -42,7 +42,6 @@ class DRSMCollection():
     self.name_col = name_col
     self.query_col = query_col
     self.mondo_col = mondo_col
-    a=1
     
   def check_query_phrase(self, phrase):
     """
